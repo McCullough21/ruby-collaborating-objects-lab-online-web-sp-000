dc865629e4ff4c9fa8289ef8f49ab4c870f9018a
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 14:43:21 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+class MP3Importer
+attr_accessor :Files
+
+def initialize(files)
+ @files = Dir.entries("mp3s")
+end
+
+def files
+  # split_at = ["-", "."]
+  #  @files.each do |file|
+  #    file.split(Regexp.union(split_at))
+  #    artist = file[0]
+end
+
+
+    
+
+
+end
