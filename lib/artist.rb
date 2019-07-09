7105a52a88533a1188a3c8404a889f33a173b3cd
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 16:39:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,33 +12,35 @@ end
 def songs
   @songs
 end
-def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
 # def self.find_or_create_by_name(name)
-#      if self.all.detect {|artist| artist.name == name}
-#        name
-#      else
-#      self.new(name)
-#      end
-# end
+#     if self.find(name)
+#       self.find(name)
+#     else
+#       self.create(name)
+#     end
+#   end
+# 
+# 
+#   def self.find(name)
+#     @@all.find do |artist|
+#       artist.name == name
+#     end
+#   end
+# 
+#   def self.create(name)
+#     artist = self.new(name)
+#     @@all << artist
+#     artist
+#   end
+def self.find_or_create_by_name(name)
+     if self.all.detect {|artist| artist.name == name}
+       name
+     else
+     @@all << self.new(name)
+     name
+     
+     end
+end
 
 def save
   @@all << self
