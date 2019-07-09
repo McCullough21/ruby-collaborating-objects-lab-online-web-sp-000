f3dd911b0a822e74f9f6ac195686c3e3c9e5365e
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 15:55:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ def initialize(name)
   @name = name
 end
 def self.new_by_filename(song_file)
-  song_file = file.split("-").collect(file[1])
+  song_file = file.chomp(".mp3").split(" - ")
   @name = file[1]
   @artist = file[0]
 end
