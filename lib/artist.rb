4fa4f70977d42dba55809d451077dd2c46737ac6
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 13:46:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,13 @@ end
 # def add_song(song)
 #   song = Song.new()
 #   song.artist =
-  
+
+def self.find_or_create_by_name(name)
+  @@all << name if !@@all.include?(name)
+end
+
+def save
+  @@all << @name
+end
 
 end
