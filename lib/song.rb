357b83da4f4f7fd428bf41fb9da606210d49025f
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 16:05:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,19 @@ attr_accessor :name, :artist
 def initialize(name)
   @name = name
 end
+
+def artist_name=(name)
+  if (self.artist.nil?)
+    self.artist = Artist.new(name)
+  else
+    self.artist.name = name
+  end
+end
+
 def self.new_by_filename(song_file)
   file = song_file.chomp(".mp3").split(" - ")
   song = Song.new(file[1])
-  song.artist.name = file[0]
+  song.artist_name(file[0])
 end
 
 
