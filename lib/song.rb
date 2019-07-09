0322f198eb2b8d8542628e9c9f1efe8268ac191d
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 15:54:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,10 @@ attr_accessor :name, :artist
 def initialize(name)
   @name = name
 end
-def self.new_by_filename(file)
-  self = file.split("-").collect(file[1])
+def self.new_by_filename(song_file)
+  song_file = file.split("-").collect(file[1])
+  @name = file[1]
+  @artist = file[0]
 end
 
 
